a3189d6787e44ded0af3586b8bb26dad6a6b5211
ozan isgor <ozanisgor11@gmail.com>
2021-01-11 15:06:02 +0300

    glitch fixed,bg img changed and clock style improved

--- day_2_clock/app.js
+++ day_2_clock/app.js
@@ -10,15 +10,21 @@ function setDate() {
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   const minutesDegrees = ((minutes / 60) * 360) + 90;
   const hoursDegrees = ((hours / 12) * 360) + 90;
+
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  if (seconds === 0) secondHand.classList.add('fast');
-  if (seconds === 1) secondHand.classList.remove('fast');
+  // for removing glitch that occurs at every 0th second
+  if (seconds === 0) secondHand.classList.add('glitch');
+  if (seconds === 1) secondHand.classList.remove('glitch');
+
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  if (minutes === 0) secondHand.classList.add('fast');
-  if (minutes === 1) secondHand.classList.remove('fast');
+  // for removing glitch that occurs at every 0th minute
+  if (minutes === 0) secondHand.classList.add('glitch');
+  if (minutes === 1) secondHand.classList.remove('glitch');
+
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  if (hours === 0) secondHand.classList.add('fast');
-  if (hours === 1) secondHand.classList.remove('fast');
+  // for removing glitch that occurs at every 0th hour
+  if (hours === 0) secondHand.classList.add('glitch');
+  if (hours === 1) secondHand.classList.remove('glitch');
 }
 
 setInterval(setDate, 1000);
