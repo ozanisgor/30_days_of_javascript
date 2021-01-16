aef93fd32b9f0898a138497038203e2bb1a21818
ozan isgor <ozanisgor11@gmail.com>
2021-01-16 14:21:08 +0300

    second challenge for map method

--- array_challenges_part_1/app.js
+++ array_challenges_part_1/app.js
@@ -34,6 +34,8 @@ console.table(filteredInventors);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullName = inventors.map(investor => `${investor.first} ${investor.last}`);
+console.table(fullName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
